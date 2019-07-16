fb016df77d5bb9dd3d29192f977ca5597896b8c4
lain <dxcrlf@gmail.com>
2019-07-17 00:04:54 +0800

    compare remote address

--- PRTS.py
+++ PRTS.py
@@ -5,6 +5,7 @@ DON'T BE EVIL
 
 import json
 import logging
+import socket
 
 from aadict import aadict
 from mitmproxy.http import HTTPFlow
@@ -15,17 +16,19 @@ from penguin_stats_report import penguin_stats_report
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%H:%M:%S')
 
+IP_ADDRESS = (socket.gethostbyname('ak-gs.hypergryph.com'), 8443)
+
 
 class PRTS:
     @staticmethod
     def response(flow: HTTPFlow):
-        if flow.request.host == 'ak-gs.hypergryph.com':
+        if flow.server_conn.ip_address == IP_ADDRESS:
             if flow.request.path == '/quest/battleFinish':
                 try:
                     response = aadict.d2ar(json.loads(flow.response.text))
                     battle_drops(response)
                     penguin_stats_report(response)
-                except AttributeError:
+                except (AttributeError, AssertionError):
                     pass
 
 
@@ -34,6 +37,7 @@ addons = [PRTS()]
 if __name__ == '__main__':
     def main():
         mitmdump(('--quiet', '--scripts', __file__))
+        # mitmdump(('--verbose', '--flow-detail', '2', '--scripts', __file__))
 
 
     main()
